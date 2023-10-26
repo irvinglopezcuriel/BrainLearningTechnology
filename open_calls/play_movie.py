bbcc52035274f098becf8073333b8d63923f5249
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2023-10-26 10:35:45 -0700

    Delete open_calls directory

--- open_calls/play_movie.py
+++
@@ -1,9 +0,0 @@
-from flask import g                                                                 
-from neurosdk.cmn_types import * 
-
-def handle_request():
-    if g.hb == None:
-        return ["Data Flowing"]
-
-    g.hb.exec_command(SensorCommand.CommandStartSignal)
-    return ["Data Flowing"]
