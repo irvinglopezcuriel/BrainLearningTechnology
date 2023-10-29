1a8716731675307d9780ce9015a541aaf0e74aaf
Exiels <Samina29@outlook.fr>
2023-10-29 14:55:30 -0700

    [ADD] Added register

--- open_calls/login.py
+++ open_calls/login.py
@@ -2,6 +2,7 @@ from flask import request, g
 from flask_json import json_response
 from tools.token_tools import create_token
 from tools.database.tables.users.get_user import get_user
+from werkzeug.exceptions import *
 
 from tools.logging import logger
 
@@ -10,6 +11,10 @@ def handle_request():
 
     email = request.form.get('email')
     password = request.form.get('password')
+    
+    if not email or not password:
+        raise BadRequest("Invalid form data")
+    
     user, role = get_user(g.cursor, email, password)
 
-    return json_response(token = create_token(user), role = role[1])
+    return json_response(status_=200, token = create_token(user), role = role[1])
---
+++ open_calls/register.py
@@ -0,0 +1,24 @@
+from flask import request, g
+from flask_json import json_response
+from tools.database.tables.users.add_user import add_user
+from tools.database.tables.roles.get_role_by_name import get_role_by_name
+from werkzeug.exceptions import *
+
+from tools.logging import logger
+
+def handle_request():
+    logger.debug("Register Handle Request")
+
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('firstname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not firstname or not lastname or not email or not password:
+        raise BadRequest("Invalid form data")
+
+    role = get_role_by_name(g.cursor, "user")
+
+    add_user(g.cursor, firstname, lastname, email, password, role[0])
+    g.db.commit()
+    return json_response(status_=200)
