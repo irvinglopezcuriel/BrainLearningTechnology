5041a6af626f4b8d55c50c9a938dbd229caaf36f
irvinglopezcuriel <ibltch@gmail.com>
2023-10-30 16:41:26 -0700

    added functionality to add task/submit feature

--- flaskLearn/app.py
+++ flaskLearn/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -20,9 +20,18 @@ class Todo(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
 	if request.method == 'POST':
-		return 'hello'
+		task_content = request.form['content']
+		new_task = Todo(content=task_content)
+
+		try:
+			db.session.add(new_task)
+			db.session.commit()
+			return redirect('/')
+		except:
+			return 'There was an issue adding task'
 	else:
-		return render_template("index.html")
+		tasks = Todo.query.order_by(Todo.date_created).all()
+		return render_template("index.html", tasks = tasks)
 
 if __name__ == "__main__":
 	app.run(debug = True)
