913cf57715426361ff2aa9b562ea1f3ee1c73c2b
Exiels <Samina29@outlook.fr>
2023-11-01 22:53:08 -0700

    [ADD] Added no_headset mode

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, redirect, g
-from flask_json import FlaskJSON, json_response
-import traceback
-from tools.eeg import get_head_band_sensor_object
+from flask_json import FlaskJSON
+from tools.eeg import get_head_band_sensor_object, init_headband
 from tools.database.db_con import get_db_instance
 from tools.database.init_db import init_db
 from tools.token_required import token_required
@@ -30,8 +29,9 @@ def init_new_env():
     if 'db' not in g:
         g.db, g.cursor = get_db_instance()
 
-    if 'hb' not in g:
-        g.hb = get_head_band_sensor_object()
+    if not bool(strtobool(os.getenv('NO_HEADSET'))):
+        if 'hb' not in g:
+            g.hb = get_head_band_sensor_object()
 
 #This gets executed by default by the browser if no page is specified
 #So.. we redirect to the endpoint we want to load the base page
@@ -69,5 +69,7 @@ def exec_proc(proc_name):
     return resp
 
 if __name__ == '__main__':
+    if not bool(strtobool(os.getenv('NO_HEADSET'))):
+        init_headband()
     init_db()
     app.run(host='0.0.0.0', port=os.getenv('FRONTEND_PORT'), use_reloader=not bool(strtobool(os.getenv('PROD'))))
--- tools/eeg.py
+++ tools/eeg.py
@@ -3,23 +3,21 @@ from neurosdk.sensor import Sensor
 from neurosdk.brainbit_sensor import BrainBitSensor
 from neurosdk.cmn_types import *
 
-from tools.logging import logger   
+from tools.logging import logger
+from distutils.util import strtobool
+import os
 
 #doing all this a the "module level" in "Demo" server mode it will work fine :)
+gl_sensor = None
+gl_scanner = None
 
 def on_sensor_state_changed(sensor, state):
     logger.debug('Sensor {0} is {1}'.format(sensor.Name, state))
 
-def on_brain_bit_signal_data_received(sensor, data):
+def on_brain_bit_signal_data_received(data):
     logger.debug(data)
 
-logger.debug("Create Headband Scanner")
-gl_scanner = Scanner([SensorFamily.SensorLEBrainBit])
-gl_sensor = None
-logger.debug("Sensor Found Callback")
-def sensorFound(scanner, sensors):
-    global gl_scanner
-    global gl_sensor
+def sensorFound(sensors):
     for i in range(len(sensors)):
         logger.debug('Sensor %s' % sensors[i])
         logger.debug('Connecting to sensor')
@@ -30,10 +28,15 @@ def sensorFound(scanner, sensors):
         gl_scanner.stop()
         del gl_scanner
 
-gl_scanner.sensorsChanged = sensorFound
+def init_headband():
+    logger.debug("Create Headband Scanner")
+    gl_scanner = Scanner([SensorFamily.SensorLEBrainBit])
+    logger.debug("Sensor Found Callback")
+    gl_scanner.sensorsChanged = sensorFound
 
-logger.debug("Start scan")
-gl_scanner.start()
+    logger.debug("Start scan")
+    gl_scanner.start()
 
 def get_head_band_sensor_object():
-    return gl_sensor
+    if not bool(strtobool(os.getenv('NO_HEADSET'))):
+        return gl_sensor
