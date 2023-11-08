32e1f777d7818fda27f23691354c222641990a29
DiegoChiok <chiok002@csusm.edu>
2023-11-08 11:44:56 -0800

    Once the survey is done, index.html is opened

--- static/js/surveypage.js
+++ static/js/surveypage.js
@@ -1,6 +1,21 @@
+//change page function to change the page
+function changePage(url) {
+    window.open(url, "_self");
+  }
+  //change page after set period of time function
+  function changePageAfter5sec(url) {
+    setTimeout(function() {
+        //call change page function
+      changePage(url)
+    }, 5000);
+  }
+  
 const survey = new Survey.Model(json);
 survey.onComplete.add((sender, options) => {
     console.log(JSON.stringify(sender.data, null, 3));
+    //switch html to the index
+    changePageAfter5sec("file:///C:/Users/jrami/OneDrive/Desktop/Github/370repo/BrainLearningTechnology/static/pages/index.html")
+
 });
 
 $("#surveyElement").Survey({ model: survey });
